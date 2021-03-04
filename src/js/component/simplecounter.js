e96a3144d4f73912a24382a23d7d8136149d58ec
josnavch <josnavch@gmail.com>
2021-03-04 00:39:04 +0000

    Finish Simple Counter JN

--- src/js/component/simplecounter.js
+++ src/js/component/simplecounter.js
@@ -1,18 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //import "./icons.js";
 
 //create your first component
-export function SimpleCounter() {
+export function SimpleCounter(props) {
 	return (
 		<div className="container">
 			<div className="clock">
 				<i className="far fa-clock"></i>
 			</div>
-			<div className="four">0</div>
-			<div className="three">0</div>
-			<div className="two">0</div>
-			<div className="one">0</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="one">{props.digitOne}</div>
 		</div>
 	);
 }
+
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.string,
+	digitThree: PropTypes.string,
+	digitTwo: PropTypes.string,
+	digitOne: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,27 @@ import "../styles/index.scss";
 import { SimpleCounter } from "./component/simplecounter.js";
 
 //render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	let four = Math.floor(counter / 1000);
+	let three = Math.floor(counter / 100);
+	let two = Math.floor(counter / 10);
+	let one = Math.floor(counter / 1);
+
+	four = four.toString().slice(-1);
+	three = three.toString().slice(-1);
+	two = two.toString().slice(-1);
+	one = one.toString().slice(-1);
+	//console.log(four, three, two, one);
+	counter++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			digitFour={four}
+			digitThree={three}
+			digitTwo={two}
+			digitOne={one}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
